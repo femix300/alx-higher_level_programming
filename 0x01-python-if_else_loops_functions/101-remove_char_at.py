f1a0acc936fdbcffd23a8743e3ace89b2858b46d
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-06-08 08:40:48 -0700

    added task 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if str is None or n is None:
+        return
+    if n >= len(str):
+        return str
+
+    mod_str = ""
+    for character in str:
+        if character != str[n]:
+            mod_str += character
+    return mod_str
