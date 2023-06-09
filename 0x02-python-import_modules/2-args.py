c371cd66e1f395a49c3d633e19415a12a81e699a
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-06-08 19:30:16 -0700

    added import guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
-i = 1
-args_passed = sys.argv[1:]
-arguments = len(sys.argv)
-arguments -= 1
-if arguments == 0:
-    print("{} arguments.".format(arguments))
-elif arguments == 1:
-    print(f"{arguments} argument:")
-    print(f"{arguments}: {sys.argv[arguments]}")
-else:
-    print(f"{arguments} arguments:")
-    for args in args_passed:
-        print(f"{i}: {args}")
-        i += 1
+if __name__ == '__main__':
+    import sys
+    i = 1
+    args_passed = sys.argv[1:]
+    arguments = len(sys.argv)
+    arguments -= 1
+    if arguments == 0:
+        print("{} arguments.".format(arguments))
+    elif arguments == 1:
+        print(f"{arguments} argument:")
+        print(f"{arguments}: {sys.argv[arguments]}")
+    else:
+        print(f"{arguments} arguments:")
+        for args in args_passed:
+            print(f"{i}: {args}")
+            i += 1
