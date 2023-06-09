8926e64dcb2c65194c859529f6ef2bb8ff846a30
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-06-08 19:28:02 -0700

    added task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+i = 1
+args_passed = sys.argv[1:]
+arguments = len(sys.argv)
+arguments -= 1
+if arguments == 0:
+    print("{} arguments.".format(arguments))
+elif arguments == 1:
+    print(f"{arguments} argument:")
+    print(f"{arguments}: {sys.argv[arguments]}")
+else:
+    print(f"{arguments} arguments:")
+    for args in args_passed:
+        print(f"{i}: {args}")
+        i += 1
