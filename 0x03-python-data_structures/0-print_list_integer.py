40efa8c1eb3df7f219df1182d41806c8931da843
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-06-17 05:13:30 -0700

    minor fix to task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
     for integer in my_list:
-        print("{}".format(int(integer)))
+        print("{}".format(integer))
