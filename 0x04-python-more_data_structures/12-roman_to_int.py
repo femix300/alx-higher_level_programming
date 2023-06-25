c68740ee6b7dcada3b2713fa86810233e60e0a7c
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-06-25 09:36:46 -0700

    added task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return None
+    previous_value = 0
+    total = 0
+    dict_of_values = {"I": 1, "V": 5, "X": 10, "L": 50,
+                      "C": 100, "D": 500, "M": 1000}
+    for char in roman_string:
+        if char in dict_of_values:
+            value = dict_of_values[char]
+            if value <= previous_value:
+                total += value
+            else:
+                total -= 2 * previous_value
+                total += value
+            previous_value = value
+    return total
