d9674301d462f18867be186cfda13fa879d3f7ec
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-06 18:47:04 -0700

    modified the logic for printing in task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,6 +50,8 @@ class Square:
         else:
             for _ in range(self.__position[1]):
                 print("")
+
             for _ in range(self.__size):
-                print(" " * self.__position[0], end='')
+                if self.__position[1] == 0:
+                    print(" " * self.__position[0], end='')
                 print("#" * self.__size)
