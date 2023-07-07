ce8a786d8b316af2f9b739faf23dd7ac51e64c65
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-06 17:01:14 -0700

    added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""defines a class square"""
+
+
+class Square:
+
+    """defines a square by private instance attribute size"""
+
+    def __init__(self, size):
+        self.__size = size
