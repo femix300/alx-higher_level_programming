96ef914e4ac8d1cf475196399a6004fbf1d15715
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-06 17:15:46 -0700

    changed isdecimal in file 2 to isinstance

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         size (int) : the size of the square
     """
     def __init__(self, size=0):
-        if not size.isdecimal():
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
