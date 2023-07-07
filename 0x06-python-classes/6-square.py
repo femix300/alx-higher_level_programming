902452be6011731475bc38690fec1d0fb0c22c05
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-06 18:44:37 -0700

    added another set of quotes in print statement int task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,7 +46,7 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print()
+            print("")
         else:
             for _ in range(self.__position[1]):
                 print("")
