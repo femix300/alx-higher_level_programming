86d0aeebfa128d95cd8af2540050526b25c7f691
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-06 17:12:55 -0700

    added file 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""defines a class"""
+
+
+class Square:
+
+    """a square initialization
+
+    Args:
+        size (int) : the size of the square
+    """
+    def __init__(self, size=0):
+        if not size.isdecimal():
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
