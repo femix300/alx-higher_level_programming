9d5be83cd5a803ccf4b507f3d086dba45720b7b0
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-17 11:09:34 -0700

    added task 0 and a readme

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a function that looks up attrubutes
+and objects"""
+
+
+def lookup(obj):
+    """returns a list of available attributes and methods"""
+
+    return dir(obj)
