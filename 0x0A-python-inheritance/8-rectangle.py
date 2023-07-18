4360a1ae21ca9a9991f901b0ddd2c77ee11fd9f5
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-17 19:41:25 -0700

    minor modes to reduce size of task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
 """
-Contains the classes BaseGeometry and Rectangle
+Contains the class Rectangle that inherits from BaseGeometery
 """
-
-
-class BaseGeometry:
-    """A class with public instance methods area and integer_validator"""
-    def area(self):
-        """raises an exception when called"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates that value is an integer that is greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """A class that inherits the class BaseGeometry"""
 
     def __init__(self, width, height):
         """validate width and height"""
-        super().__init__()
-        self.integer_validator("width", width)
+        self.integer_validator("width", width) that inherits from BaseGeometery
         self.integer_validator("height", height)
 
         self.__width = width
