1019907d48de1d53904408abd5bdefc99e300fdb
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-17 19:32:22 -0700

    added task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Contains the classes BaseGeometry and Rectangle
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer that is greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A class that inherits the class BaseGeometry"""
+
+    def __init__(self, width, height):
+        super().__init__()
+
+        """validate width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
