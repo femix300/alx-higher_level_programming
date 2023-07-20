20cbc5344a2d240869c1d06365142f42d913cc29
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 07:24:51 -0700

    made task 8 achieve the same thing in a more efficient manner

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,13 +5,5 @@
 
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
-    for JSON serialization of an object"""
-    if isinstance(obj, dict):
-        return {key: class_to_json(value) for key, value in obj.items()}
-    elif isinstance(obj, list):
-        return [class_to_json(item) for item in obj]
-    if hasattr(obj, '__dict__'):
-        return {key: class_to_json(value)
-                for key, value in obj.__dict__.items()}
-
-    return obj
+    fon JSON serialization of an object"""
+    return obj.__dict__
