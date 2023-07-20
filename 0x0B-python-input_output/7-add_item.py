9660c73ecc7c5ed4ed9b329ef39827d7856371b5
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 06:52:03 -0700

    fixed pycodestyle for task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,10 +4,11 @@
 import json
 import sys
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == '__main__':
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     try:
         data = load_from_json_file("add_item.json")
     except FileNotFoundError:
