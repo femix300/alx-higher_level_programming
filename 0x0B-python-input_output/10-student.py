7facb588d68a9388093d3edac005ebd35bcdfe05
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 08:24:54 -0700

    another way of task 10 documentation and minor changes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a class student"""
+"""Defines a class Student."""
 
 
 class Student:
-    """Represents a student."""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
@@ -17,11 +17,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs):
+    def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
 
         If attrs is a list of strings, represents only those attributes
@@ -30,7 +26,7 @@ class Student:
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-        if isinstance(attrs, list) and
-        all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs}
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
