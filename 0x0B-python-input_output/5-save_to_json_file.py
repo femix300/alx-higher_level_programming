a8e576e504700da3b72e3b2aead21352b12ccba7
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 01:20:02 -0700

    imported necessary modules

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Defines a JSON representation function"""
-
+import json
 
 def save_to_json_file(my_obj, filename):
     """A function that writes an Object to a text file,
