d071b9bf06e14e7bbc86b7ff62e8e9d8dbb00c18
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 08:07:28 -0700

    final mods to documentation for task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 
-"""Defines a Student class"""
+"""Defines a Student Class"""
 
 
 class Student:
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+    """
+    Represents a student.
 
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+    Args:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance
-        Args:
-            attrs (list of str): A list of attribute names to retrieve.
-        Returns:
-            dict: A dictionary containing the specified
-            attributes and their values.
+    def to_json(self, attrs):
         """
-        if isinstance(attrs, list) and
-        all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(obj, attr) for attr in attrs}
+        Retrieves a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list of str): List of attribute names to retrieve.
+
+        Returns:
+            dict: Dictionary containing the specified attributes and their values.
+
+        If 'attrs' list is empty, it returns a dictionary representation
+        of all attributes of the Student instance.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs}
         return self.__dict__
