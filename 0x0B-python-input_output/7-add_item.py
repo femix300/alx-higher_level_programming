a25e9fb5be121eb320ce790e4ca910d9db33997c
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 06:48:54 -0700

    import sys in task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 
 """Adds all arguments to a Python list, and then save them to a file"""
 import json
-
+import sys
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
