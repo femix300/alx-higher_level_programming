ee67185e9e0fb2238ce2b24aee0135b10a14a05f
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 07:52:58 -0700

    added more documentation for task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,13 +4,14 @@
 
 
 class Student:
-    """Retrieves a dictionary representation of a Student instance"""
+    """Initialization of the student class"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
         if isinstance(attrs, list) and
         all(isinstance(attr, str) for attr in attrs):
             return {attr: getattr(obj, attr) for attr in attrs}
