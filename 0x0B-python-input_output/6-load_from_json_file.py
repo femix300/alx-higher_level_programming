97268306bd5122d65d7032862bdc5c5c51918210
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 01:31:57 -0700

    added task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a JSON object creating function"""
+import json
+
+
+def load_from_json_file(filename):
+    """A function that creates an Object from a 'JSON file'"""
+    with open(filename, "w") as file:
+        return json.load(file)
