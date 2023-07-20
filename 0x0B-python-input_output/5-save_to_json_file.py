eceba6a493037ec67acaaec415ec91ffa038ccb3
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 01:23:35 -0700

    used parameters correctly and added pycodestyle fix to task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,6 +3,7 @@
 """Defines a JSON representation function"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """A function that writes an Object to a text file,
     using a JSON representation
@@ -11,4 +12,4 @@ def save_to_json_file(my_obj, filename):
         obj (Any): The Python object to be written to the file.
     """
     with open(filename, "w") as file:
-        json.dump(obj, file)
+        json.dump(my_obj, file)
