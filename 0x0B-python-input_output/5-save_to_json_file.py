45d5d1577985d1125226364ab301ea2eaea1d86a
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-20 01:16:40 -0700

    added task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a JSON representation function"""
+
+
+def save_to_json_file(my_obj, filename):
+    """A function that writes an Object to a text file,
+    using a JSON representation
+    Args:
+        filename (str): The path of the file to be written.
+        obj (Any): The Python object to be written to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(obj, file)
