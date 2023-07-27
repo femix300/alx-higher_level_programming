88d190fbc9fb8367269868c2b7c608740502cf26
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-27 04:59:00 -0700

    added task 11 a getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,15 @@ class Square(Rectangle):
         """Initializes a new square"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """A string representation of the square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
