a9c2da1f0fe2b637f1656844cd34be21377148dd
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-27 06:26:44 -0700

    added task 17 which returns the list of a json string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,6 +33,13 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation 'json_string' """
+        if not json_string:
+            return "[]"
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file"""
