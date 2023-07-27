b1668e603161655fb71e092c4908e9b437834224
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-07-27 05:49:34 -0700

    added the static method for task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if not list_dictionaries:
