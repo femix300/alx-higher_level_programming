c128a978b457262cc42c0f082d95f6ad9fc3c3fc
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-16 09:49:33 +0100

    changed the code in task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,2 @@
 -- A script that lists all the tables of a database.
-USE information_schema;
-
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'information_schema';
+SHOW TABLES;
