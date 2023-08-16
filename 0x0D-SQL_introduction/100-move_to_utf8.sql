a24bf5c3742ba0aaef313e5cc39e55ef597161df
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-16 10:31:46 +0100

    added task 100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,10 @@
+-- A script that converts hbtn_0c_0 database to UTF8
+
+-- convert Database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- covert TABLE
+ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- convert FIELD
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
