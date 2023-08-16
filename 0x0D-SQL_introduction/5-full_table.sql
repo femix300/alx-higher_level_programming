2d93b1d3d042aa7ab24c14ec6aa948d25c013379
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-16 04:00:03 +0100

    fin

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0
-SELECT GROUP_CONCAT(
-    CONCAT(column_name, ' ', column_type, ' ', IF(is_nullable = 'NO', 'NOT NULL', ''), ' ',
-           IF(column_default IS NOT NULL, CONCAT('DEFAULT ', column_default), ''))
-    SEPARATOR ',\n  '
-) AS create_table
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-    AND table_name = 'first_table';
+SHOW CREATE TABLE `first_table`;
