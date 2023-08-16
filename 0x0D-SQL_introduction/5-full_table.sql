ddcb84f2fe66481fe260064edae92deb202361b2
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-16 03:50:30 +0100

    changed the the code in task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+FROM information_schema.columns
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+    AND TABLE_NAME = 'first_table';
