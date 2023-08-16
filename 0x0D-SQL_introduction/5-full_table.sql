62a9dcb3239e2f2ed3ae93dbf5080f8736090c50
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-08-16 03:53:40 +0100

    modified task/file 5 again

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,9 @@
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+SELECT GROUP_CONCAT(
+    CONCAT(column_name, ' ', column_type, ' ', IF(is_nullable = 'NO', 'NOT NULL', ''), ' ',
+           IF(column_default IS NOT NULL, CONCAT('DEFAULT ', column_default), ''))
+    SEPARATOR ',\n  '
+) AS create_table
 FROM information_schema.columns
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-    AND TABLE_NAME = 'first_table';
+WHERE table_schema = 'hbtn_0c_0'
+    AND table_name = 'first_table';
