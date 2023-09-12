f36b50a7ba61d1aed85637de22951597d2315da4
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-12 02:42:54 +0100

    changed the logic in task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,19 +5,16 @@ let sec = 0;
 
 if (process.argv.length <= 3) {
   console.log('0');
-} else if (process.argv.length === 4) {
-  if (process.argv[2] > process.argv[3]) {
-    sec = process.argv[3];
-  } else {
-    sec = process.argv[2];
-  }
-  console.log(sec);
 } else {
   for (let i = 2; i < process.argv.length; i++) {
-    const curr = process.argv[i];
-    if (curr > max) {
-      sec = max;
-      max = curr;
+    const curr = parseInt(process.argv[i], 10);
+    if (!(isNaN(curr))) {
+      if (curr > max) {
+        sec = max;
+        max = curr;
+      } else if (curr > sec && curr !== max) {
+        sec = curr;
+      }
     }
   }
   console.log(sec);
