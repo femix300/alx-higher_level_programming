7f0d7e0487a0b19a7bf3153738ed1851c849b5fc
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-12 03:43:20 +0100

    added task 103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  /*
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
   YOUR CODE HERE
   */
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+
+myObject.incr = function incr () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
