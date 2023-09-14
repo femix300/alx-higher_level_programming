86c8c497584c5c2689d5bf885f685ca4637cae7b
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-14 04:07:10 +0100

    added task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,14 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
+  }
 
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
   }
 }
 
