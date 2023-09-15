d41d163146c3f043945f460e9575d41d61831394
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-15 11:26:31 +0100

    changed the logic to convert command line input to strings

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,25 +1,9 @@
 #!/usr/bin/node
 
+// Import fs module
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.log('Usage: node concat.js <file1> <file2> <output_file>');
-  process.exit(1);
-}
-
-const file1 = process.argv[2];
-const file2 = process.argv[3];
-const outputFile = process.argv[4];
-
-// Read the contents of file1 (if it exists)
-const data1 = fs.existsSync(file1) ? fs.readFileSync(file1, 'utf8') : '';
-const data2 = fs.readFileSync(file2, 'utf8');
-
-const concatenatedData = (data1 !== '' ? data1 + '\n' : '') + data2;
-
-fs.writeFile(outputFile, concatenatedData, 'utf8', (err) => {
-  if (err) {
-    process.exit(1);
-  }
-console.log(concatenatedData);
-});
+const fileA = fs.readFileSync(process.argv[2]).toString();
+const fileB = fs.readFileSync(process.argv[3]).toString();
+const concatenatedContent = fileA + fileB;
+fs.writeFileSync(process.argv[4], concatenatedContent);
