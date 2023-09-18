c7feca86a194201d1d126d17d6bdc2d0f57e2ff0
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-18 13:42:59 +0100

    added appropriate comments in task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument."""
+"""
+Takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa
+"""
 
 import MySQLdb
 import sys
