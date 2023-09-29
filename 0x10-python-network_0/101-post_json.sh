bf70bbbb4c5575450d5c28777a61ab078bdf8b9b
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-29 22:02:49 +0100

    modified the code in task 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends a JSON POST request, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+curl -X POST -H "Content-Type: application/json" --data "@$2" "$1"
+
