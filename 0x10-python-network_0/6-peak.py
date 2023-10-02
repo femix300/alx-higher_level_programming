15e062cfc650c2278c207cf6607540c2087c1996
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-10-02 08:57:41 +0100

    added files back and changed time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,22 @@
-O(log(n))
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """A function that finds a peak in a list of unsorted integers"""
+
+    array = list_of_integers
+
+    left, right = 0, len(array) - 1
+
+    while left <= right:
+        mid = left + ((right - left) // 2)
+
+        # check if left neighbor is greater
+        if mid > 0 and array[mid] < array[mid - 1]:
+            right = mid - 1
+        # check if right neighbor is greater
+        elif mid < len(array) - 1 and array[mid] < array[mid + 1]:
+            left = mid + 1
+        else:
+            return array[mid]
