e9ed20fd047106947da584767c205986e6c199b0
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-10-30 16:45:27 +0100

    modified the code in task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,16 @@
 #!/usr/bin/node
-const fs = require('fs');
+
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
