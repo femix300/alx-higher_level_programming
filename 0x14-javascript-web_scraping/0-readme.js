8718a5fbc9cb0878ba033faa2d352d6a1812da13
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-10-30 14:54:52 +0100

    modified the code in task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
