ef3e52b653c4acf85a13d91152ba01eaac63f8d9
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-11-01 10:36:03 +0100

    modified the code in task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
+const url = 'https://jsonplaceholder.typicode.com/todos';
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+  const tasks = JSON.parse(body);
+
+  const completedTasks = {};
+
+  for (const task of tasks) {
+    if (task.completed) {
+      const userId = task.userId;
+      if (completedTasks[userId]) {
+        completedTasks[userId]++;
+      } else {
+        completedTasks[userId] = 1;
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
+
+  console.log(completedTasks);
 });
