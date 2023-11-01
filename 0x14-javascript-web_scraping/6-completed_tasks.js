b29a80ab98f7ca2505bcf6f5216552a10e62da76
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-11-01 10:41:42 +0100

    modified the code to get response from the command line

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://jsonplaceholder.typicode.com/todos';
+const url = process.argv[2];
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
